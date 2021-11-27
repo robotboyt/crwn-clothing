23b161cd14d03b5d0ec0dca1ffb62e994724ac5c
robotboyt <bokachodantes@gmail.com>
2021-11-27 16:55:59 +0200

    uodate menu item withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import './App.css';
-
+import { Switch, Route } from "react-router-dom"
 import HomePage from './components/pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+  Hello BABABUE
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
