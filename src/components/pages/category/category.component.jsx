d7cd26973934db214c8a11e1e9e1debe52221db9
robotboyt <bokachodantes@gmail.com>
2021-12-01 11:54:50 +0200

    added nested routing in shop page

---
+++ src/components/pages/category/category.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import collectionItemComponent from '../../collection-item/collection-item.component'
+
+import './category.styles.scss'
+
+const CategoryPage = ({ match }) => { 
+    console.log(match);
+    return(
+    <div className="category">
+        <h2>CATEGORY PAGE</h2>
+    </div>
+)}
+
+export default CategoryPage
