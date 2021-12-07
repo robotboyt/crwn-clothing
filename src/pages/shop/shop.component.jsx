f139443e4217544e869b3fdef2772aa2dae58b0a
robotboyt <bokachodantes@gmail.com>
2021-12-07 20:56:04 +0200

    added spinner component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -9,10 +9,20 @@ import {
 
 import { updateCollections } from '../../redux/shop/shop.actions';
 
+import WithSpinner from '../../components/with-spinner/with-spinner.comonent.jsx';
+
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
+const CollectionsOverviewWithSpinner  = WithSpinner(CollectionsOverview)
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
+
 class ShopPage extends React.Component {
+
+  state = {
+    loadding: true
+  }
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
@@ -22,17 +32,19 @@ class ShopPage extends React.Component {
     collectionRef.get().then((snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      this.setState({loadding: false})
     });
   }
 
   render() {
     const { match } = this.props;
+    const { loadding } = this.state
     return (
       <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner  isLoading={loadding} {...props} />} />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={(props) => <CollectionPageWithSpinner isLoading={loadding} {...props}/>}
         />
       </div>
     );
